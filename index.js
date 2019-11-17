413ac8bfd3860531532e02696fc2ca8919af5c02
frenz064 <57821956+frenz064@users.noreply.github.com>
2019-11-16 22:18:56 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 $(document).ready(function(){
 
  $("#wordfinder").on('click', function (e) {
