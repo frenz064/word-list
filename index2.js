a4f8220680a1726b9472255fbcbdd0b1648eaa97
frenz064 <57821956+frenz064@users.noreply.github.com>
2019-11-17 13:09:46 -0600

    Create index2.js

---
+++ index2.js
@@ -0,0 +1,14 @@
+document.getElementById("wordfinder");
+
+
+/*$(document).ready(function(){
+ $("#wordfinder").on('click', function (e) {
+ 	e.preventDefault();
+  // verify data is correctly submitted then call functions to edit results.html
+  var $form = $(this),url=$form.attr('action');
+	var serialize = $("wordfinder").serialize();
+ 	var posting = $.post("results.html", serialize);
+  /*var $inputs = $("#wordfinder:inputs").serializeArray();
+  e.preventDefault();*/
+ //});
+//});
