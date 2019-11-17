f64b9df93382a94900664c4ac00d8ee990b9fbf6
frenz064 <57821956+frenz064@users.noreply.github.com>
2019-11-17 11:45:42 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 
 //create a server object:
 
-+http.createServer(function (req, res) {
+http.createServer(function (req, res) {
   fs.readFile('index.html', function(err,data) {
 	 res.writeHead(200,{'Content-Type': 'text/html'});
 	 res.write(data);
