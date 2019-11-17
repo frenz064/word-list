6deba6d217c3adffd814be8c238b55a8da223ef4
frenz064 <57821956+frenz064@users.noreply.github.com>
2019-11-16 22:52:00 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var http = require('http');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(process.env.PORT || 5000);
+
 $(document).ready(function(){
 
  $("#wordfinder").on('click', function (e) {
