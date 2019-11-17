b7cd9f58e407b6bf2abf9a26d0de555c13e782d2
frenz064 <57821956+frenz064@users.noreply.github.com>
2019-11-17 12:07:36 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var http = require('http');
 var fs = require('fs');
 
+var html = fs.readFileSync('index.html');
+
 var server = http.createServer(function (req, res) {
-  fs.readFile('index.html', function(err,data) {
-	 res.write(data);
-	res.end();
-  	});
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.end(html);
 }).listen(process.env.PORT || 5000);
 
 
