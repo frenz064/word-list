a2d6a5f7dc43390c13f51dd3769685a5e8b9a82b
frenz064 <57821956+frenz064@users.noreply.github.com>
2019-11-17 13:09:15 -0600

    Update index.js

--- index.js
+++ index.js
@@ -7,19 +7,3 @@ var server = http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.end(html);
 }).listen(process.env.PORT || 5000);
-
-document.getElementById("wordfinder");
-
-
-/*$(document).ready(function(){
- $("#wordfinder").on('click', function (e) {
- 	e.preventDefault();
-  // verify data is correctly submitted then call functions to edit results.html
-  var $form = $(this),url=$form.attr('action');
-	var serialize = $("wordfinder").serialize();
- 	var posting = $.post("results.html", serialize);
-  /*var $inputs = $("#wordfinder:inputs").serializeArray();
-  e.preventDefault();*/
- //});
-//});
-
