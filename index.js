3012f78ff7e37cc9de110464fd96ef3925062b68
frenz064 <57821956+frenz064@users.noreply.github.com>
2019-11-17 11:16:16 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,23 +3,16 @@ var fs = require('fs');
 
 //create a server object:
 var server = http.createServer(function (req, res) {
-  		fs.readFile('./'+ res.url, function(err,data) {
-	 		var dotoffset = req.url.lastIndexOf('.');
-            		var mimetype = dotoffset == -1
-                            ? 'text/plain'
-                            : {
-                                '.html' : 'text/html',
-                                '.ico' : 'image/x-icon',
-                                '.jpg' : 'image/jpeg',
-                                '.png' : 'image/png',
-                                '.gif' : 'image/gif',
-                                '.css' : 'text/css',
-                                '.js' : 'text/javascript'
-                                }[ req.url.substr(dotoffset) ];
-            res.setHeader('Content-type' , mimetype);
-            res.end(data);
-            console.log( req.url, mimetype );
-  		});
+  		switch (req.url) {
+        		case "/index.css" :
+            res.writeHead(200, {"Content-Type": "text/css"});
+            res.write(cssFile);
+            break;
+        default :    
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.write(htmlFile);
+    });
+    res.end();
 	      }).listen(process.env.PORT || 5000);
 
 /*$(document).ready(function(){
