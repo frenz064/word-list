b5eefa335640e38be1272dab08e8cdf02bb4a12d
frenz064 <tonkakels@gmail.com>
2019-11-17 11:04:56 -0600

    Revert "Update index.js"
    This reverts commit a1f8428312916539736b9928214239129b3f6fb9.

--- index.js
+++ index.js
@@ -2,16 +2,19 @@ var http = require('http');
 var fs = require('fs');
 
 //create a server object:
-fs.readFileSync('index.html', function(err,data) {
-	if (err) {
-		throw err;
-	}
-	http.createServer(function (req, res) {
- 		res.writeHeader(200,{'Content-Type': 'text/html'});
-		res.write(data);
+http.createServer(function (req, res) {
+  fs.readFile('index.html', function(err,data) {
+	 res.writeHead(200,{'Content-Type': 'text/html'});
+	 res.write(data);
+	 /*fs.readFile('index.css', function(err2,data2) {
+		res.writeHead(200,{"Content-Type": "text/css"});
+		res.write(data2);*/
 		res.end();
-	}).listen(process.env.PORT || 5000);
+   	// });
   });
+
+  
+}).listen(process.env.PORT || 5000);
 
 /*$(document).ready(function(){
 
